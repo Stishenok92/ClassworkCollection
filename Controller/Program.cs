85be8b214a4671116efc49d16f57e54834f637c3
Stishenok92 <Stishenok92@gmail.com>
2020-11-25 00:21:45 +0300

    Start

---
+++ Controller/Program.cs
@@ -0,0 +1,15 @@
+﻿using static Model.BusinessLogic;
+using static View.Printer;
+using static Util.UserInput;
+using System;
+
+namespace Controller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
